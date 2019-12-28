4d59584d1759ce852c53aa49e565283ce4fb0d77
e.n.dutkiewicz@gmail.com <e.n.dutkiewicz@gmail.com>
2019-12-28 19:03:34 +0100

    init commit

---
+++ src/atj/WebSocketEndpoint.java
@@ -0,0 +1,49 @@
+package atj;
+
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ApplicationScoped
+@ServerEndpoint("/chatendpoint")
+
+public class WebSocketEndpoint {
+	private static final ConcurrentLinkedQueue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+	@OnOpen
+	public void onOpen(Session session) {
+		sessions.add(session);
+	}
+
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		System.out.println(message);
+		for (Session aSession:sessions) {
+			if(!aSession.equals(session)){
+				try {
+					aSession.getBasicRemote().sendText(message);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+	
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason){
+		sessions.remove(session);
+	}
+	@OnError
+	public void onError(Throwable error) {}
+	
+
+}
